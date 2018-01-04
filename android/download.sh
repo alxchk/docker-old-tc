824971401bdb8796b8d37f690bd6f6974c6dde7e
Oleksii Shevchuk <ol.shevchuk@samsung.com>
2018-01-04 16:52:31 +0200

    Add android (experemental)

---
+++ android/download.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+mkdir downloads
+wget --no-check-certificate -c https://github.com/alxchk/python-for-android/archive/master.zip -O downloads/python-for-android.zip & \
+wget --no-check-certificate -c https://github.com/kivy/buildozer/archive/master.zip -O downloads/buildozer.zip & \
+wget --no-check-certificate -c http://archive.apache.org/dist/ant/binaries/apache-ant-1.9.4-bin.tar.gz -O downloads/apache-ant-1.9.4-bin.tar.gz & \
+wget --no-check-certificate -c http://dl.google.com/android/android-sdk_r20-linux.tgz -O downloads/android-sdk_r20-linux.tgz & \
+wget --no-check-certificate -c http://dl.google.com/android/ndk/android-ndk-r9c-linux-x86_64.tar.bz2 -O downloads/android-ndk-r9c-linux-x86_64.tar.bz2
+
+
