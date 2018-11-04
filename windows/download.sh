7654b741b651805196c07dd242cb06edb84ae7b0
Oleksii Shevchuk <alxchk@gmail.com>
2018-11-04 18:53:59 +0200

    windows: revert prebuilding opus using mingw

--- windows/download.sh
+++ windows/download.sh
@@ -8,10 +8,9 @@ PYTHON64="https://www.python.org/ftp/python/$PYVER/python-$PYVER.amd64.msi"
 PYTHON32="https://www.python.org/ftp/python/$PYVER/python-$PYVER.msi"
 PYTHONVC="https://download.microsoft.com/download/7/9/6/796EF2E4-801B-4FC4-AB28-B59FBF6D907B/VCForPython27.msi"
 WINETRICKS="https://raw.githubusercontent.com/Winetricks/winetricks/master/src/winetricks"
-OPUS_SRC="https://archive.mozilla.org/pub/opus/opus-1.3.tar.gz"
 
 mkdir -p $DOWNLOADS
 
-for dist in $PYTHON32 $PYTHON64 $PYTHONVC $WINETRICKS $OPUS_SRC; do
+for dist in $PYTHON32 $PYTHON64 $PYTHONVC $WINETRICKS; do
     wget -qcP $DOWNLOADS $dist
 done
